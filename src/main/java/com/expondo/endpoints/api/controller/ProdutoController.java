33b0f106c1d67c90248a4accfbaa19e655f00cb9
Lucas da Costa <49735129+lucascostadevrobot@users.noreply.github.com>
2022-09-27 15:24:54 -0300

    Constrói metodos GET, POST, DELETE classe ProdutoController

--- src/main/java/com/expondo/endpoints/api/controller/ProdutoController.java
+++ src/main/java/com/expondo/endpoints/api/controller/ProdutoController.java
@@ -4,10 +4,11 @@ package com.expondo.endpoints.api.controller;
 import com.expondo.endpoints.api.domain.Produto;
 import com.expondo.endpoints.api.repository.ProdutoRepository;
 import com.expondo.endpoints.api.service.ProdutoService;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/produto")
@@ -15,7 +16,6 @@ public class ProdutoController {
 
     @Autowired
      private ProdutoService produtoService;
-     private ProdutoRepository produtoRepository;
      private Produto produto;
 
     //1º Metodo é pegar o produto e salvar o mesmo no BD
@@ -36,7 +36,14 @@ public class ProdutoController {
     //Metodo para deletar via ID
     @DeleteMapping(path = {"/{id}"})
     public ResponseEntity<Produto> deletaProduto(@PathVariable Long id){
-        Produto deletaProduto = produtoService.findById(id);
-        return ResponseEntity.ok().body(deletaProduto);
+        Produto produto = produtoService.findById(id);
+        return ResponseEntity.ok().body(produto);
+    }
+
+    //Metodo para listar todos os produtos
+    @GetMapping(value = "/listar-todos")
+    public ResponseEntity<List<Produto>> buscarTodos(){
+        List<Produto> produtos = produtoService.findAll();
+        return ResponseEntity.ok().body(produtos);
     }
 }
